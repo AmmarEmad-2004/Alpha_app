3fc09c05109b8225bbf146e61d2a39774045207d
AmmarEmad-2004 <za0662003@email.com>
2025-03-15 17:14:35 +0200

    initialize Flutter application structure and add platform-specific configurations

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AlphaApp());
+}
+
+class AlphaApp extends StatelessWidget {
+  const AlphaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
