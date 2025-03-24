47e83df2a06d1bfce3683cda0d9138a40cccfc10
AmmarEmad-2004 <za0662003@email.com>
2025-03-24 15:43:53 +0200

    create buttom navigation bar and finish home view details

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,17 +1,41 @@
 import 'package:alpha_app/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: HomeViewBody(),
 
-      // bottomNavigationBar: BottomNavigationBar(
-      //   // items: [BottomNavigationBarItem(icon: Icons.home)],
-      // ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Saved'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.teal,
+        onTap: _changeIndex,
+      ),
     );
+  }
+
+  void _changeIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
