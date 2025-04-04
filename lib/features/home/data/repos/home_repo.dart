064acce18b3bee21452bc3b501efb2609ab8a5fc
AmmarEmad-2004 <za0662003@email.com>
2025-04-04 17:25:39 +0200

    add Failuers class for error handling and implement HomeRepo interface for book category and retrieval operations

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,11 @@
+import 'package:alpha_app/core/errors/api_failuer.dart';
+import 'package:alpha_app/core/errors/failuers.dart';
+import 'package:alpha_app/features/home/data/models/book_model/book_model.dart';
+import 'package:alpha_app/features/home/data/models/book_model/category_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failuers,List<CategoryModel>>> getCategoryBooks();
+  Future<Either<ApiFailure,List<BookModel>>> getBooksByCategory(String categoryName);
+  Future<Either<ApiFailure,List<BookModel>>> getNewArrivalsBooks();
+}
