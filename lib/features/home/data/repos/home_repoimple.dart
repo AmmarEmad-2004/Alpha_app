0249ccc9296e688a8d56b236efd3bad6caf5afe2
AmmarEmad-2004 <za0662003@email.com>
2025-04-04 17:50:38 +0200

    refactor HomeRepoimple to utilize ApiService for fetching books by category and improve error handling

--- lib/features/home/data/repos/home_repoimple.dart
+++ lib/features/home/data/repos/home_repoimple.dart
@@ -1,34 +1,58 @@
 import 'package:alpha_app/core/errors/api_failuer.dart';
 import 'package:alpha_app/core/errors/failuers.dart';
+import 'package:alpha_app/core/utils/api_service.dart';
 import 'package:alpha_app/features/home/data/models/book_model/book_model.dart';
 import 'package:alpha_app/features/home/data/models/book_model/category_model.dart';
 import 'package:alpha_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoimple implements HomeRepo{
+class HomeRepoimple implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoimple({required this.apiService});
   @override
-  Future<Either<ApiFailure, List<BookModel>>> getBooksByCategory(String categoryName) {
-    // TODO: implement getBooksByCategory
-    throw UnimplementedError();
+  Future<Either<ApiFailure, List<BookModel>>> getBooksByCategory(
+    String categoryName,
+  ) async {
+    try {
+      var data = await apiService.get(
+        endPoint:
+            'volumes?q=subject:$categoryName&filter=full&maxResults=20&orderBy=newest',
+      );
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(ServerFailure(message: e.toString()));
+      }
+    }
   }
 
   @override
-  Future<Either<Failuers, List<CategoryModel>>> getCategoryBooks() async{
+  Future<Either<Failuers, List<CategoryModel>>> getCategoryBooks() async {
     try {
-  List<CategoryModel> categories = [
-      CategoryModel(categoryName: 'Fiction'),
-      CategoryModel(categoryName: 'Science'),
-      CategoryModel(categoryName: 'Literature'),
-      CategoryModel(categoryName: 'History'),
-      CategoryModel(categoryName: 'Business'),
-      CategoryModel(categoryName: 'Technology'),
-      CategoryModel(categoryName: 'Travel'),
-      CategoryModel(categoryName: 'Health'),
-    ];
-    return Right(categories);
-}  catch (e) {
-  return Left(Failuers(message: 'Failed to fetch categories'));
-}
+      List<CategoryModel> categories = [
+        CategoryModel(categoryName: 'Fiction'),
+        CategoryModel(categoryName: 'Science'),
+        CategoryModel(categoryName: 'Literature'),
+        CategoryModel(categoryName: 'History'),
+        CategoryModel(categoryName: 'Business'),
+        CategoryModel(categoryName: 'Technology'),
+        CategoryModel(categoryName: 'Travel'),
+        CategoryModel(categoryName: 'Health'),
+      ];
+      return right(categories);
+    } catch (e) {
+      return left(Failuers(message: 'Failed to fetch categories'));
+    }
   }
 
   @override
